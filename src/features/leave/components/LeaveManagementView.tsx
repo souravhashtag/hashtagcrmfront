39933a008d28fafa4a35fbf0f04857ed882dc05d
souravhashtag <souravg.hashtag@gmail.com>
2025-08-06 16:42:19 +0530

    Assign work - 06-08-2025 -2

--- src/features/leave/components/LeaveManagementView.tsx
+++ src/features/leave/components/LeaveManagementView.tsx
@@ -12,7 +12,6 @@ import { useNavigate } from 'react-router-dom';
 import {
   useGetAllLeavesQuery,
 
-  useUpdateLeaveStatusMutation,
   useDeleteLeaveMutation,
   useUpdateLeaveStatusMutation
 } from '../../../services/leaveServices';
